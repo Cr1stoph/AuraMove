14b0245f11ebff6e2f397eb52d2894aa39f2814c
Cr1stoph <roamedelc@gmail.com>
2026-04-13 09:20:29 -0400

    archivo intializr auramove

---
+++ auramove/src/main/java/com/duoc/auramove/AuramoveApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.auramove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuramoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuramoveApplication.class, args);
+	}
+
+}
---
+++ auramove/src/test/java/com/duoc/auramove/AuramoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.auramove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuramoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
