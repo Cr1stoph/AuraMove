a067640c934fe801c520648eeb7f91b8e31f0c09
Cr1stoph <roamedelc@gmail.com>
2026-04-23 13:29:44 -0400

    cambios actuales

--- auramove/src/main/java/com/duoc/auramove/model/Rutina.java
+++ auramove/src/main/java/com/duoc/auramove/model/Rutina.java
@@ -6,12 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-<<<<<<< HEAD
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-=======
->>>>>>> ca3b531de4b7106dc878b19e8c7a90fbe31d56d1
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
