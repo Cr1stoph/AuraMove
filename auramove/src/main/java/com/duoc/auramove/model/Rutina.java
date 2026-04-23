c9ec5cc8e6c361c5a2dc318d4ef32dda27b316a3
JValencia04 <ja.valenciaa@duocuc.cl>
2026-04-23 12:54:08 -0400

    Creacion usuarioRepository

--- auramove/src/main/java/com/duoc/auramove/model/Rutina.java
+++ auramove/src/main/java/com/duoc/auramove/model/Rutina.java
@@ -1,14 +1,16 @@
 package com.duoc.auramove.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -18,4 +20,9 @@ public class Rutina {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    private String nombre;
+    private String descripcion;
+    private Integer nivel;
+    private String tipo_rutina;
 }
--- auramove/src/main/java/com/duoc/auramove/repository/pruebaRepository.java
+++
@@ -1,5 +0,0 @@
-package com.duoc.auramove.repository;
-
-public class pruebaRepository {
-    
-}
