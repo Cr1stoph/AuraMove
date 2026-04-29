aa6b748d8fbe3f986a6f3acb8b34d2862fc7fec4
Cr1stoph <roamedelc@gmail.com>
2026-04-29 15:33:59 -0400

    Clase ejercicio actualizada

--- auramove/src/main/java/com/duoc/auramove/model/Ejercicio.java
+++ auramove/src/main/java/com/duoc/auramove/model/Ejercicio.java
@@ -1,5 +1,27 @@
 package com.duoc.auramove.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
 public class Ejercicio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+   
+    @NotBlank
+    private String nombreUsuario;
+    
+    @NotBlank
+    private String email;
+   
+    @NotBlank
+    private String password;
+    
+    @NotBlank
+    private String nivel;
+
     
 }
