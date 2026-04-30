8419c0a0b5baaff3e77351fccc0daff02a230db5
Cr1stoph <roamedelc@gmail.com>
2026-04-30 12:38:17 -0400

    CreacionApiError para manejo de errores en exception

---
+++ auramove/src/main/java/com/duoc/auramove/exception/ApiError.java
@@ -0,0 +1,12 @@
+package com.duoc.auramove.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiError {
+    private int codigo;
+    private String mensaje;
+    private String detalle;
+}
