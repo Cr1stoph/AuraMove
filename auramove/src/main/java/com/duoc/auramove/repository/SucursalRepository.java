db2098a69e89f162b60cef9dd703deee7f2f62ba
Cr1stoph <roamedelc@gmail.com>
2026-04-30 12:58:41 -0400

    cambio a jparepoitory de sucursal

---
+++ auramove/src/main/java/com/duoc/auramove/repository/SucursalRepository.java
@@ -0,0 +1,10 @@
+package com.duoc.auramove.repository;
+
+import com.duoc.auramove.model.Sucursal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
+    
+}
