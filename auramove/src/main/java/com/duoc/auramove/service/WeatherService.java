0579d6c6100ff49f70cb963f410afb8a56af26a2
JValencia04 <ja.valenciaa@duocuc.cl>
2026-05-06 15:51:53 -0400

    Agrego Weather

---
+++ auramove/src/main/java/com/duoc/auramove/service/WeatherService.java
@@ -0,0 +1,24 @@
+package com.duoc.auramove.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.duoc.auramove.Dto.WeatherDTO;
+
+@Service
+public class WeatherService {
+
+    @Autowired
+    @Qualifier("weatherWebClient")
+    private WebClient weatherWebClient;
+
+    public WeatherDTO obtenerClima(double latitude, double longitude) {
+        return weatherWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1/forecast")
+                        .queryParam("latitude", latitude)
+                )
+    }
+}
