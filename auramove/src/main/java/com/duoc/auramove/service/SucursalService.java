57ed57ec0d34711bd47361fc44b81a8e92b06154
Cr1stoph <roamedelc@gmail.com>
2026-05-15 11:53:24 -0400

    Comentarios necesarios para entender sucursal servcie

--- auramove/src/main/java/com/duoc/auramove/service/SucursalService.java
+++ auramove/src/main/java/com/duoc/auramove/service/SucursalService.java
@@ -13,19 +13,22 @@ public class SucursalService {
     public List<Sucursal> getSucursales() {
         return sucursalRepository.findAll();
     }
-
+    // Guarda una nueva sucursal
     public Sucursal saveSucursal(Sucursal sucursal){
         return sucursalRepository.save(sucursal);
     }
+    //Obtiene sucursal por id
     public Sucursal getSucursalById(Integer id) {
         return sucursalRepository.findById(id).orElse(null);
     }
+    // Actualiza sucursal si es que existe
     public Sucursal updateSucursal(Sucursal sucursal) {
         if(!sucursalRepository.existsById(sucursal.getId())) {
             return null;
         }
         return sucursalRepository.save(sucursal);
     }
+// Eliminar sucursal
     public void deleteSucursal(Integer id){
         sucursalRepository.deleteById(id);
     }
