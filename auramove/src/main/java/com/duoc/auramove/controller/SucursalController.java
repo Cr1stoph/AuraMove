271ca737529bdaed685b0d765b4989a3e1d94cc2
Cr1stoph <roamedelc@gmail.com>
2026-05-15 12:03:56 -0400

    get mapping de Sucursal

--- auramove/src/main/java/com/duoc/auramove/controller/SucursalController.java
+++ auramove/src/main/java/com/duoc/auramove/controller/SucursalController.java
@@ -1,10 +1,13 @@
 package com.duoc.auramove.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.duoc.auramove.service.SucursalService;
+import com.duoc.auramove.model.Sucursal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sucursales")
@@ -12,4 +15,12 @@ public class SucursalController {
     
     @Autowired
     private SucursalService sucursalService;
+
+    @GetMapping
+     public ResponseEntity<List<Sucursal>> listarSucursales(){
+        System.out.println(("[SucursalController] -> listarSucursales"));
+        return ResponseEntity.ok(sucursalService.getSucursales());
+    }
+    
+
 }
