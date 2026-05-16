cf48488605cd1002381778a7ec433a224f2f3ef6
Cr1stoph <roamedelc@gmail.com>
2026-05-16 19:37:41 -0400

    SucursalController Terminado

--- auramove/src/main/java/com/duoc/auramove/controller/SucursalController.java
+++ auramove/src/main/java/com/duoc/auramove/controller/SucursalController.java
@@ -1,13 +1,20 @@
 package com.duoc.auramove.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.duoc.auramove.service.SucursalService;
 import com.duoc.auramove.model.Sucursal;
 import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/sucursales")
@@ -21,6 +28,35 @@ public class SucursalController {
         System.out.println(("[SucursalController] -> listarSucursales"));
         return ResponseEntity.ok(sucursalService.getSucursales());
     }
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<Sucursal> getSucursalById(@PathVariable Integer id){
+        System.out.println("[SucursalController] -> getSucursalById id = "+id);
+        Sucursal sucursal = sucursalService.getSucursalById(id);
+        if(sucursal == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(sucursal);
+    }
+    @PostMapping
+    public ResponseEntity<Sucursal> saveSucursal(@Valid @RequestBody Sucursal sucursal){
+        System.out.println("[SucursalController] -> saveUsuario");
+        return ResponseEntity.status(HttpStatus.CREATED).body(sucursalService.saveSucursal(sucursal));   
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Sucursal> updateUsuario(@PathVariable Integer id, @Valid @RequestBody Sucursal sucursal){
+        System.out.println("[UsuarioController] -> updateUsuario id = "+id);
+        sucursal.setId(id);
+        Sucursal actualizado = sucursalService.updateSucursal(sucursal);
+        if(actualizado == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(actualizado);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeSucursal(@PathVariable Integer id){
+        System.out.println("[SucursalController] -> removeSucursal id = "+ id);
+        sucursalService.deleteSucursal(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
