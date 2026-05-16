fdd1673d3ee327ac8c4015d01af0a693881f6c61
Cr1stoph <roamedelc@gmail.com>
2026-05-16 19:19:28 -0400

    UsuarioController listo

--- auramove/src/main/java/com/duoc/auramove/controller/UsuarioController.java
+++ auramove/src/main/java/com/duoc/auramove/controller/UsuarioController.java
@@ -1,10 +1,25 @@
 package com.duoc.auramove.controller;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.duoc.auramove.model.Usuario;
 import com.duoc.auramove.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -13,5 +28,43 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAllusuarios(){
+        System.out.println("[UsuarioController] -> getAllUsuarios");
+        return ResponseEntity.ok(usuarioService.getUsuarios());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Integer id){
+        System.out.println("[UsuarioController] -> getUsuarioById id =" + id);
+        Usuario usuario = usuarioService.getUsuarioById(id);
+        if (usuario == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> saveUsuario(@Valid @RequestBody Usuario usuario){
+        System.out.println("[UsuarioController] -> saveUsuario");
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.saveUsuario(usuario));
+    }
+        @PutMapping("/{id}")
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable Integer id, @Valid @RequestBody Usuario usuario){
+        System.out.println("[UsuarioController] -> updateUsuario id =" + id);
+        usuario.setId(id);
+        Usuario actualizado = usuarioService.updateUsuario(usuario);
+        if(actualizado == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(actualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeUsuario(@PathVariable Integer id){
+        System.out.println("[UsuarioController] -> removeUsuario id =" + id);
+        usuarioService.deleteUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
