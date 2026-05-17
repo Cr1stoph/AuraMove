3db6ed2a18af0a8252a65f133df14a0fbe041927
Cr1stoph <roamedelc@gmail.com>
2026-05-16 20:54:34 -0400

    Cambio de metodos privados a publicos en ejercicio service

--- auramove/src/main/java/com/duoc/auramove/service/EjercicioService.java
+++ auramove/src/main/java/com/duoc/auramove/service/EjercicioService.java
@@ -13,26 +13,26 @@ public class EjercicioService {
     private EjercicioRepository ejerciciorepository;
 
     // listar los ejercicios
-    private List<Ejercicio> getEjercicios(){
+    public List<Ejercicio> getEjercicios(){
         return ejerciciorepository.findAll();
     }
     //Guardar nuevo ejercicio
-    private Ejercicio saveEjercicio(Ejercicio ejercicio){
+    public Ejercicio saveEjercicio(Ejercicio ejercicio){
         return ejerciciorepository.save(ejercicio);
     }
     // Buscar ejercicio por id
-    private Ejercicio getEjercicioById(Integer id){
+    public Ejercicio getEjercicioById(Integer id){
         return ejerciciorepository.findById(id).orElse(null);
     }
     //Actualiza nuevo ejercicio
-    private Ejercicio updateEjercio(Ejercicio ejercicio){
+    public Ejercicio updateEjercio(Ejercicio ejercicio){
         if (!ejerciciorepository.existsById(ejercicio.getId())) {
             return null;
         }
         return ejerciciorepository.save(ejercicio);
     }
     //Eliminar ejercicio
-    private void deleteEjercicio(Integer id){
+    public void deleteEjercicio(Integer id){
         ejerciciorepository.deleteById(id);
     }
 
