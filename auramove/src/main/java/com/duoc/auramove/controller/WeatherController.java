a246f4f78e021cbf92758f649c9488a360e2ef47
Cr1stoph <roamedelc@gmail.com>
2026-05-18 09:04:44 -0400

    Weather controller terminado

---
+++ auramove/src/main/java/com/duoc/auramove/controller/WeatherController.java
@@ -0,0 +1,28 @@
+package com.duoc.auramove.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duoc.auramove.Dto.WeatherDTO;
+import com.duoc.auramove.service.WeatherService;
+
+@RestController
+@RequestMapping("/api/v1/cliente")
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping
+    public ResponseEntity<WeatherDTO> clima(
+        @RequestParam(defaultValue = "-33.45") double lat,
+        @RequestParam(defaultValue = "-70.65") double lon){
+            System.out.println("[WeatherController] -> clima lat = "+ lat + ", lon = " + lon);
+            WeatherDTO resultado = weatherService.obtenerClima(lat, lon);
+            return ResponseEntity.ok(resultado);
+        }
+}
