c2d8d7225793e5b49fe8157dd0f76bf0cd00b21c
Cr1stoph <roamedelc@gmail.com>
2026-05-18 09:23:51 -0400

    Agrego nuevos atributos para Usuario y agrego mensajes en las validaciones

--- auramove/src/main/java/com/duoc/auramove/model/Usuario.java
+++ auramove/src/main/java/com/duoc/auramove/model/Usuario.java
@@ -2,9 +2,6 @@ package com.duoc.auramove.model;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,10 +9,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,20 +24,26 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
-    @NotBlank
+    @NotBlank(message = "Debe escribir su nombre")
     private String nombre;
+
+    @NotBlank(message = "Debe colocar su apellido")
+    private String apellido;
     
-    @Column(unique = true)
-    @NotBlank
+    @NotBlank(message = "El email es obligatorio")
     private String email;
 
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    private String contraseña;
+
+    @NotNull(message = "La edad es obligatoria")
+    private Integer edad;
+
     //Un usuario puede tener muchas sucursales y cada sucursal pertenece a un único usuario
-    @JsonIgnore
     @OneToMany(mappedBy = "usuario")
     private List<Sucursal> sucursales;
 
     //Un usuario tiene muchas rutinas pero cada rutina pertenece a un usuario
-    @JsonIgnore
     @OneToMany(mappedBy = "usuario")
     private List<Rutina> rutinas;
 }
