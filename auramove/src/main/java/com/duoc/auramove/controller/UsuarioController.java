bf9d484904b6c2a904ea792587c7e3babc2c7de1
Cr1stoph <roamedelc@gmail.com>
2026-05-19 19:02:31 -0400

    Elimino imports que no se usan

--- auramove/src/main/java/com/duoc/auramove/controller/UsuarioController.java
+++ auramove/src/main/java/com/duoc/auramove/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package com.duoc.auramove.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +12,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.duoc.auramove.Dto.UserEmailDTO;
 import com.duoc.auramove.model.Usuario;
 import com.duoc.auramove.service.UsuarioService;
-
 import jakarta.validation.Valid;
-
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -64,6 +61,11 @@ public class UsuarioController {
         System.out.println("[UsuarioController] -> removeUsuario id =" + id);
         usuarioService.deleteUsuario(id);
         return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/com-email")
+    public ResponseEntity<List<UserEmailDTO>> userPorEmail(){
+        System.out.println("[UsuarioController] -> userPorEmail");
+        return ResponseEntity.ok(usuarioService.getUsuarioConEmail());
     }
 
 }
