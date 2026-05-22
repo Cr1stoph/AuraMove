038ac207762001e33f9edc4c94da887eb44f70c0
Cr1stoph <roamedelc@gmail.com>
2026-05-22 00:01:04 -0400

    Actualizo metodo de guardar nueva rutina para DTO Rtutina

--- auramove/src/main/java/com/duoc/auramove/service/RutinaService.java
+++ auramove/src/main/java/com/duoc/auramove/service/RutinaService.java
@@ -2,23 +2,41 @@ package com.duoc.auramove.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.duoc.auramove.Dto.RutinaDTO;
 import com.duoc.auramove.model.Rutina;
+import com.duoc.auramove.model.Usuario;
 
 import java.util.List;
 
 import com.duoc.auramove.repository.RutinaRepository;
+import com.duoc.auramove.repository.UsuarioRepository;
 
 @Service
 public class RutinaService {
     @Autowired
     private RutinaRepository rutinaRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    
     //Obtener lista ed todas las sucursales
     public List<Rutina> getRutina(){
         return rutinaRepository.findAll();
     }
-    //Registrar rutina
-    public Rutina saveRutina(Rutina rutina){
-        return rutinaRepository.save(rutina);
+
+    public Rutina saveRutina(RutinaDTO dto) {
+        // 1. Buscamos al usuario
+        Usuario usuario = usuarioRepository.findById(dto.getUsuarioId())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID: " + dto.getUsuarioId()));
+        Rutina nuevaRutina = new Rutina();
+        nuevaRutina.setNombre(dto.getNombre());
+        nuevaRutina.setDescripcion(dto.getDescripcion());
+        nuevaRutina.setNivel(dto.getNivel());
+        nuevaRutina.setTipoRutina(dto.getTipoRutina());
+
+        nuevaRutina.setUsuario(usuario);
+        return rutinaRepository.save(nuevaRutina);
     }
     //Obtener rutina por id
     public Rutina getRutinaById(Integer Id){
